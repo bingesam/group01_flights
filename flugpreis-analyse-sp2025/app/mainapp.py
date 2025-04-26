e3b5229b27ad445f76907fba94f7d612fb319d5f
finocgio <finocgio@students.zhaw.ch>
2025-04-26 11:39:57 +0000

    gf

--- flugpreis-analyse-sp2025/app/mainapp.py
+++ flugpreis-analyse-sp2025/app/mainapp.py
@@ -1,52 +1,34 @@
 import requests
-import pandas as pd
-import re
-from scipy.stats import ttest_ind
 import os
 from dotenv import load_dotenv
 
+# Token laden
 load_dotenv()
 TOKEN = os.getenv("TRAVELPAYOUTS_TOKEN")
 
 def fetch_flight_data(origin="FRA", destination="BCN", currency="EUR"):
-    #url = "https://api.travelpayouts.com/v1/prices/calendar"
     url = "https://api.travelpayouts.com/v2/prices/latest"
+    
+    headers = {
+        "X-Access-Token": TOKEN
+    }
+    
     params = {
         "origin": origin,
         "destination": destination,
-        "currency": currency,
-        "token": TOKEN
+        "currency": currency
     }
-    response = requests.get(url, params=params)
+    
+    response = requests.get(url, headers=headers, params=params)
+    print("Status Code:", response.status_code)
+    print("Antworttext:", response.text)
+
+    print("Status:", response.status_code)
+    print("Antwort:", response.text[:300])
+    
+    if response.status_code != 200:
+        print("❗ API Fehler")
+        return []
+    
     data = response.json()
-    return data["data"]
-
-def prepare_dataframe(api_data):
-    # Struktur in pandas bringen
-    records = []
-    for date, info in api_data.items():
-        price = info.get("price")
-        if price:
-            records.append({
-                "date": date,
-                "price": price,
-                "transfers": info.get("transfers", 0)
-            })
-    df = pd.DataFrame(records)
-    df['date'] = pd.to_datetime(df['date'])
-    return df
-
-def analyze_price_distribution(df):
-    return df.describe()
-
-def compare_weekday_prices(df):
-    df["weekday"] = df["date"].dt.day_name()
-    weekdays = df.groupby("weekday")["price"].mean()
-    return weekdays
-
-def run_statistical_test(df):
-    df["weekday"] = df["date"].dt.day_name()
-    mon = df[df["weekday"] == "Monday"]["price"]
-    fri = df[df["weekday"] == "Friday"]["price"]
-    t_stat, p_val = ttest_ind(mon, fri, equal_var=False)
-    return t_stat, p_val
+    return data.get("data", [])
